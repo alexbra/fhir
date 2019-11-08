98e06cd100dd546241a840eb5df6761225e99c0e
Alex Brazhenko <alex.brazhenko@xealth.io>
2019-11-07 22:55:16 -0800

    final

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -7,7 +7,7 @@ export default class Patient extends React.Component {
     super(props);
     this.state = {
       loading: true,
-      patient: null,
+      patient: {},
       error: null
     };
   }
@@ -42,37 +42,29 @@ export default class Patient extends React.Component {
     };
 
     try {
-      // const result = await oauth2.authorizationCode.getToken(options);
+      const result = await oauth2.authorizationCode.getToken(options);
       // console.log('result', result)
-      const result = {
-        access_token: "UJO-ycW0Iunal01hoMSn6o6MNIZehJiBfXJoTwxI9tdWFk_HADzaHe-YhNcR9JTE7wvuajFmtr0ejXg5QNb3pzssDRwVm_wBT44tkIaQypWrPFhd_iATHgN9WD9s4jds",
-        encounter: "TCbcjwKK3IOU9mCTss9wi8m-E-EpVfXQ6bTTzrh5HbIIB",
-        expires_in: 3600,
-        id_token: "eyJhbGciOiJSUzI1NiIsImtpZCI6Ik9pZUlRYW04MGRhSStSbWMrZ3VqS2FaRnJ2dUx0c05OVDl2MHZ1ZjRGOTQ9IiwidHlwIjoiSldUIn0.eyJhdWQiOiJhMTljZmU4MC00M2Y2LTQyYjAtOTU0ZS03ODRmNzcxYmRiNzIiLCJleHAiOjE1NzMxOTU0OTAsImlhdCI6MTU3MzE5NTE5MCwiaXNzIjoiaHR0cHM6Ly9hcHBvcmNoYXJkLmVwaWMuY29tL2ludGVyY29ubmVjdC1hb2N1cnByZC1vYXV0aC9vYXV0aDIvdG9rZW4iLCJzdWIiOiJlQ0s0dXRwUE43MVlMUy1oSFR0WTdFUTMifQ.CVueIvSAx7Nyd8dZir2CUEgEoaBAm7kExwRm-K9MSnRVoDJodrulowKXw0TU8UFBLGte4x8gJ1tD7_rH2Y97JARN7OCBGFrfhpBz1xGdgjagBu9DrrksYh0kFlwn2Gx9YNj5-MidCkW1Oe3U51JyyJPNBZu4pyznyM9fkVf4GT5sSyHTNH94YHMl4LHdctPOEHTSOGOYucZApcOT-bP_qGL7IEaswQXfv2x8AvhRRP0F84ulOop-xuovR2qJ27WKb2_97jClIOljpBttrTR-UB4J9X9mLuzevXj6ZJPlDIXH_02A0AjmF65XhJ415X-GWdRu4DTlb9VpEnC_5_fqyg",
-        location: "TJAAnzPV3aoaXFJTg6bautgB",
-        need_patient_banner: "false",
-        patient: "TVMN5ZaLn5SPfIwGoAGoGUBrG8w0AWoxyNbE7F-EPJ2MB",
-        scope: "launch openid PATIENT.READ Patient.search Patient.Read (STU3) Patient.Read (R4)",
-        smart_style_url: "https://apporchard.epic.com/interconnect-aocurprd-oauth/api/epic/2016/EDI/HTTP/style/102202/I0YwRjJGNHwjQzEyMTI3fCMxQUFCRkZ8I0QzRDhERXwjODZCNTQwfCMwMDAwMDB8MHB4fDEwcHh8fEFyaWFsLCBzYW5zLXNlcmlmfCdTZWdvZSBVSScsIEFyaWFsLCBzYW5zLXNlcmlmfHw%3D.json",
-        state: "12345",
-        token_type: "bearer",
-        "__epic.dstu2.patient": "TVMN5ZaLn5SPfIwGoAGoGUBrG8w0AWoxyNbE7F-EPJ2MB"
-      }
+
       const { token } = oauth2.accessToken.create(result);
 
       console.log('The token is : ', token);
 
       fhirClient.bearerToken = token.access_token;
-      console.log(fhirClient)
       const patient = await fhirClient.read({ resourceType: 'Patient', id: token.patient });
+      console.log(patient)
 
-      console.log('patient', patient)
+      this.setState({patient})
     } catch (error) {
       console.error('Access Token Error', error.message);
     }
   }
   render() {
     const { error, loading, patient } = this.state
-    return "Patient"
+    return (
+      <div>
+        {JSON.stringify(this.state.patient, null, 2)}
+      </div>
+    )
+
   }
 }
