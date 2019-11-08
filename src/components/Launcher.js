dcbc98ab4e18478ee1c907a572858a9f3aea9e10
Alex Brazhenko <alex.brazhenko@xealth.io>
2019-11-07 18:06:49 -0800

    one more

--- src/components/Launcher.js
+++ src/components/Launcher.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Client from 'fhir-kit-client'
 import simpleOauthModule from 'simple-oauth2'
+import { oauth2 as SMART } from "fhirclient"
+
 /**
  * Typically the launch page is an empty page with a `SMART.authorize`
  * call in it.
@@ -19,32 +21,39 @@ export default class Launcher extends React.Component {
   async componentDidMount() {
     const CLIENT_ID = 'a19cfe80-43f6-42b0-954e-784f771bdb72';
     const iss = 'https://apporchard.epic.com/interconnect-aocurprd-oauth/api/FHIR/DSTU2'
-    const fhirClient = new Client({ baseUrl: iss })
-    const { authorizeUrl, tokenUrl } = await fhirClient.smartAuthMetadata();
-    const oauth2 = simpleOauthModule.create({
-      client: {
-        id: CLIENT_ID,
-      },
-      auth: {
-        tokenHost: `${tokenUrl.protocol}//${tokenUrl.host}`,
-        tokenPath: tokenUrl.pathname,
-        authorizeHost: `${authorizeUrl.protocol}//${authorizeUrl.host}`,
-        authorizePath: authorizeUrl.pathname,
-      },
-      options: {
-        authorizationMethod: 'body',
-      },
-    });
+    // const fhirClient = new Client({ baseUrl: iss })
+    // const { authorizeUrl, tokenUrl } = await fhirClient.smartAuthMetadata()
+    // console.log(authorizeUrl)
+    // const oauth2 = simpleOauthModule.create({
+    //   client: {
+    //     id: CLIENT_ID,
+    //   },
+    //   auth: {
+    //     tokenHost: `${tokenUrl.protocol}//${tokenUrl.host}`,
+    //     tokenPath: tokenUrl.pathname,
+    //     authorizeHost: `${authorizeUrl.protocol}//${authorizeUrl.host}`,
+    //     authorizePath: authorizeUrl.pathname,
+    //   },
+    //   options: {
+    //     authorizationMethod: 'body',
+    //   },
+    // });
 
-    // Authorization uri definition
-    const authorizationUri = oauth2.authorizationCode.authorizeURL({
-      redirect_uri: 'http://localhost:3000/callback',
-      launch: '',
-      aud: iss,
-      scope: 'launch openid profile user/Patient.read patient/*.*',
-      state: '3(#0/!~',
-    });
-    console.log(authorizationUri)
+    // // Authorization uri definition
+    // const authorizationUri = oauth2.authorizationCode.authorizeURL({
+    //   redirect_uri: 'http://localhost:3555/callback',
+    //   launch: '',
+    //   aud: iss,
+    //   scope: 'launch openid profile user/Patient.read patient/*.*',
+    //   state: '3(#0/!~',
+    // });
+    // console.log(authorizationUri)
+    SMART.authorize({
+      clientId: CLIENT_ID,
+      scope: "launch launch/patient patient/read offline_access",
+      redirectUri: "./app",
+      iss
+    })
   }
   /**
    * Could also return `null` for empty page
